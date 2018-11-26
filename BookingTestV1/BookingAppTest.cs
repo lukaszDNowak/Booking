148a55a5400e48e5de21054781ead506f5c6e220
Lukasz Nowak <Lukasz.d.nowak@interia.pl>
2018-11-26 21:29:53 +0100

    .

--- BookingTestV1/BookingAppTest.cs
+++ BookingTestV1/BookingAppTest.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace BookingTestV1    
+namespace BookingTest   
 {  
     [TestFixture] 
     public class BookingAppTest
